79ca0df1c3cb6553394c9451d5d4e2405aba699d
Hqrrg <harry.howes@student.norwichuni.ac.uk>
2024-09-23 14:35:13 +0100

    Initial commit

---
+++ Source/CheeseChase/CheeseChaseGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CheeseChaseGameMode.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACheeseChaseGameMode::ACheeseChaseGameMode()
+{
+}
---
+++ Source/CheeseChase/CheeseChaseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CheeseChaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACheeseChaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACheeseChaseGameMode();
+};
+
+
+
